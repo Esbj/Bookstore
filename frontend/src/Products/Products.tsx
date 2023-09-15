6ca2e6aa0ec87abe132ab8b48ee5d8de9f8b88de
Alexander Esbjörnsson <alex.esbj@gmail.com>
2023-09-15 14:32:27 +0200

    lifted state of HeadingToggle

--- frontend/src/Products/Products.tsx
+++ frontend/src/Products/Products.tsx
@@ -1,9 +1,12 @@
+import { useState } from "react";
 import HeadingToggle from "../common/HeadingToggle/HeadingToggle";
 
 export default function Products() {
+  const [selectedTab, setSelectedTab] = useState('Books');
+  const updateSelectedTab = (value: string) => { setSelectedTab(value) }
   return (
     <>
-      <HeadingToggle />
+      <HeadingToggle selectedTab={selectedTab} updateSelectedTab={updateSelectedTab} />
     </>
   )
 }
--- frontend/src/common/HeadingToggle/HeadingToggle.tsx
+++ frontend/src/common/HeadingToggle/HeadingToggle.tsx
@@ -1,13 +1,17 @@
-import { useState } from 'react';
-import './headingToggle.scss'
 import Typography from '@mui/material/Typography';
-export default function HeadingToggle() {
-  const [selectedTab, setSelectedTab] = useState('Books');
+import './headingToggle.scss'
+
+type Props = {
+  selectedTab: string,
+  updateSelectedTab: (value: string) => void
+}
+export default function HeadingToggle({ selectedTab, updateSelectedTab }: Props) {
+
 
   return (
     <div className='toggleWrapper'>
-      <Typography variant="h2" className={selectedTab === 'Books' ? 'selected' : ''} onClick={() => setSelectedTab('Books')}>Books</Typography>
-      <Typography variant="h2" className={selectedTab === 'Authors' ? 'selected' : ''} onClick={() => setSelectedTab('Authors')}>Authors</Typography>
+      <Typography variant="h2" className={selectedTab === 'Books' ? 'selected' : ''} onClick={() => updateSelectedTab('Books')}>Books</Typography>
+      <Typography variant="h2" className={selectedTab === 'Authors' ? 'selected' : ''} onClick={() => updateSelectedTab('Authors')}>Authors</Typography>
     </div>
   )
 }
