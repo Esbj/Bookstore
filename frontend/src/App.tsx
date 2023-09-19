97a354ed30bf92455ceb12a37763387e6d06a294
Alesa <alesa@live.se>
2023-09-19 11:02:27 +0200

    bookshelf done + mobileversion

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,19 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Layout from './Layout/Layout'
 import Products from './Layout/Products/Products'
+import Home from './Layout/Home/Home'
 
 function App() {
   return (
+    <>
     <Routes>
       <Route path="/" element={<Layout />} />
       <Route path="/products" element={<Products />} />
     </Routes>
+    <Home/>
+    </>
   )
+  
 }
 
 export default App
---
+++ frontend/src/Layout/Home/Home.tsx
@@ -0,0 +1,45 @@
+import { Link } from "react-router-dom";
+import "./Home.scss";
+import Button from '@mui/material/Button';
+import bookImage from './bookimage.jpeg';
+import secondBook from './saga.jpg';
+import bookIcon from './books.jpg'
+
+
+
+function Home() {
+    return (
+        <>
+        
+            <div className='frame-container'>
+                <div className='round-corner'>
+                    
+                <div className="bookIcon">
+                        <img src={bookIcon} alt="Book" className="book-icon" /></div>
+                    <h2 className='title'>New & <br />Trending</h2>
+                    <p className="desc">Explore new worlds from authors</p>
+
+                    <Link to="/products" style={{ textDecoration: 'none' }}>
+                        <Button variant="text" style={{ borderRadius: '50px', 
+                        width: '200px', backgroundColor: '#FFFFFF', color: '#9D918B' }}>
+                            Browse all books
+                        </Button>
+                    </Link>
+
+                    <div className="bookshelf">
+                        <img src={bookImage} alt="Book" className="book-image" />
+                        <img src={secondBook} alt="Another Image" className="another-image" />
+                    </div>
+                    
+
+                    
+                </div>
+            </div>
+            
+           
+        </>
+    );
+}
+
+export default Home;
+
