cf027bb5f1eac40f20acb641bce2c56c81f27954
Alexander Esbjörnsson <alex.esbj@gmail.com>
2023-09-22 17:10:57 +0200

    Fixed homepage on smaller desktop screens

--- frontend/src/Layout/Home/Home.tsx
+++ frontend/src/Layout/Home/Home.tsx
@@ -11,23 +11,26 @@ function Home() {
       <div className="bookIcon">
         <img src={bookIcon} alt="book-logo" className="book-icon" />
       </div>
+      <div className="content">
 
-      <div className="text">
-        <h2 className='title'>New & <br />Trending</h2>
-        <p className="desc">Explore new worlds from authors</p>
+        <div className="text">
+          <h2 className='title'>New & <br />Trending</h2>
+          <p className="desc">Explore new worlds from authors</p>
 
-        <Link to="/products" style={{ textDecoration: 'none' }}>
-          <Button variant="text" style={{
-            borderRadius: '50px',
-            width: '200px', backgroundColor: '#FFFFFF', color: '#9D918B'
-          }}>
-            Browse all books
-          </Button>
-        </Link>
-      </div>
-      <div className="bookshelf">
-        <img src={bookImage} alt="Book" className="book-image" />
-        <img src={secondBook} alt="Another Image" className="another-image" />
+          <Link className="button" to="/products" style={{ textDecoration: 'none' }}>
+            <Button variant="text" style={{
+              borderRadius: '50px',
+              width: '200px', backgroundColor: '#FFFFFF', color: '#9D918B'
+            }}>
+              Browse all books
+            </Button>
+          </Link>
+        </div>
+
+        <div className="bookshelf">
+          <img src={bookImage} alt="Book" className="book-image" />
+          <img src={secondBook} alt="Another Image" className="another-image" />
+        </div>
       </div>
     </div>
   );
