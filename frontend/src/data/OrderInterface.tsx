22d0304c3f5b239b96fd88f937fc949fe1865c30
Qendresa Shala <80218438+Alesa3@users.noreply.github.com>
2023-09-22 12:41:28 +0200

    Update OrderInterface.tsx

--- frontend/src/data/OrderInterface.tsx
+++ frontend/src/data/OrderInterface.tsx
@@ -1,18 +1,15 @@
 import { Book } from "./BookInterface";
 
 export interface Order {
-
-    firstName: string;
-    lastName: string;
-    address: { zip: string; streetAddress: string; city: string };
-    phoneNumber: string;
-    email: string;
-    books: Book[];
-    totalPrice: number;
-    shippingMethod: { [key: string]: number };
-    paymentMethod?: { [key: string]: string };
-    status?: string;
-    _id: number;
+  firstName: string;
+  lastName: string;
+  address: { zip: string; streetAddress: string; city: string };
+  phoneNumber: string;
+  email: string;
+  books: Book[];
+  totalPriceWithShipping: number;
+  status?: string;
+}
 
 export interface ShippingMethod {
   shippingMethod: {
