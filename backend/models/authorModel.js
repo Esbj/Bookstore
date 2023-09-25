a0b121065477a4f62afdd649d2e6ce53577313ef
Alexander Esbjörnsson <alex.esbj@gmail.com>
2023-09-25 15:54:57 +0200

    added new route for books by a author

--- backend/models/authorModel.js
+++ backend/models/authorModel.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
 
-
-
 const AuthorSchema = mongoose.Schema({
     name: {
         type: String,
--- backend/routes/author.js
+++ backend/routes/author.js
@@ -5,18 +5,21 @@ const authorModel = require('../models/authorModel');
 const bookModel = require('../models/bookModel');
 
 authorRouter.get('/', async (req, res) => {
+  const authors = await authorModel.find();
+  res.json(authors)
+});
+
+
+authorRouter.get('/:id/books', async (req, res) => {
   try {
-    const authors = await authorModel.find();
-    const authorsWithBooks = [];
-    for (const author of authors) {
-      const books = await bookModel.find({ author: author.name });
-      const authorWithBooks = { ...author.toObject(), books };
-      authorsWithBooks.push(authorWithBooks);
-    }
-    res.status(200).json(authorsWithBooks);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Internal server error' });
+    const id = req.params.id;
+    const author = await authorModel.findById(id)
+    const books = await bookModel.find({ author: author.name });
+    res.json(books)
+  }
+  catch (error) {
+    console.error(error)
+    res.status(500).json({ message: `Error: ${error}` })
   }
 });
 
