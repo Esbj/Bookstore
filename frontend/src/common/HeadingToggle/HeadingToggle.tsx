6d62219a650f19b2e1fdc011f8eb223fd80bb2ba
Alexander Esbjörnsson <alex.esbj@gmail.com>
2023-09-26 16:40:51 +0200

    heading toggle modular

--- frontend/src/common/HeadingToggle/HeadingToggle.tsx
+++ frontend/src/common/HeadingToggle/HeadingToggle.tsx
@@ -3,15 +3,16 @@ import './headingToggle.scss'
 
 type Props = {
   selectedTab: string,
-  updateSelectedTab: (value: string) => void
+  updateSelectedTab: (value: string) => void,
+  tab1: string,
+  tab2: string
 }
-export default function HeadingToggle({ selectedTab, updateSelectedTab }: Props) {
 
-
+export default function HeadingToggle({ selectedTab, updateSelectedTab, tab1, tab2 }: Props) {
   return (
-    <div className='toggleWrapper'>
-      <Typography variant="h2" className={selectedTab === 'Books' ? 'selected' : ''} onClick={() => updateSelectedTab('Books')}>Books</Typography>
-      <Typography variant="h2" className={selectedTab === 'Authors' ? 'selected' : ''} onClick={() => updateSelectedTab('Authors')}>Authors</Typography>
+    <div className="toggleWrapper">
+      <Typography variant="h2" className={selectedTab === tab1 ? 'selected' : ''} onClick={() => updateSelectedTab(tab1)}>{tab1}</Typography>
+      <Typography variant="h2" className={selectedTab === tab2 ? 'selected' : ''} onClick={() => updateSelectedTab(tab2)}>{tab2}</Typography>
     </div>
   )
 }
