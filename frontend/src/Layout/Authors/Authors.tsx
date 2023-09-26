2e12bd9c8cd1d96d4daec5ff53bc170f5e1eb13f
Alexander Esbjörnsson <alex.esbj@gmail.com>
2023-09-26 11:00:30 +0200

    Author rendering complete

--- frontend/src/Layout/Authors/Authors.tsx
+++ frontend/src/Layout/Authors/Authors.tsx
@@ -3,6 +3,8 @@ import useFetch from "../../customHooks/useFetch"
 import "./Authors.scss"
 import Grid from '@mui/material/Unstable_Grid2';
 import { Author } from "../../data/BookInterface"
+import { Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 export default function Authors() {
   const url = "http://localhost:3000/author"
   const fetchAuthors = useFetch(url)
@@ -17,7 +19,11 @@ export default function Authors() {
     <Grid container spacing={3}>
       {authors?.map((author, index) => (
         <Grid xs={2} md={4} key={index} className="authorCard">
-          {author.name}
+          <img src={author.imageUrl} alt={author.name} />
+          <Link to={`/author/${author._id}`}>
+            <Typography variant="h3">{author.name}</Typography>
+          </Link>
+
         </Grid>
       ))}
     </Grid>
