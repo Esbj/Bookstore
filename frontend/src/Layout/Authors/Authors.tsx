9d123554fd8e2f27f5bf5354e7e4f7bea8d07196
Alexander Esbjörnsson <alex.esbj@gmail.com>
2023-09-26 10:37:25 +0200

    rendering authors from db

--- frontend/src/Layout/Authors/Authors.tsx
+++ frontend/src/Layout/Authors/Authors.tsx
@@ -1,6 +1,25 @@
+import { useMemo } from "react"
+import useFetch from "../../customHooks/useFetch"
 import "./Authors.scss"
+import Grid from '@mui/material/Unstable_Grid2';
+import { Author } from "../../data/BookInterface"
 export default function Authors() {
+  const url = "http://localhost:3000/author"
+  const fetchAuthors = useFetch(url)
+  const { data, isLoading, error } = useMemo(() => fetchAuthors, [fetchAuthors])
+  const authors = data as Author[]
+
+
+  error && error.message
+  isLoading && "Loading..."
+
   return (
-    <div>Authors</div>
+    <Grid container spacing={3}>
+      {authors?.map((author, index) => (
+        <Grid xs={2} md={4} key={index} className="authorCard">
+          {author.name}
+        </Grid>
+      ))}
+    </Grid>
   )
 }
