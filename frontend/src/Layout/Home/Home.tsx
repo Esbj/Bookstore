b909a026d5335cabc3db98e43a52f3f737f004a0
Alexander Esbjörnsson <alex.esbj@gmail.com>
2023-09-28 13:59:48 +0200

    books on startpage

--- frontend/src/Layout/Home/Home.tsx
+++ frontend/src/Layout/Home/Home.tsx
@@ -5,7 +5,19 @@ import bookImage from './bookimage.jpeg';
 import secondBook from './saga.jpg';
 import bookIcon from '../../assets/books-logo.svg';
 import LoginRoundedIcon from '@mui/icons-material/LoginRounded';
+import Grid from '@mui/material/Unstable_Grid2';
+import useFetch from "../../customHooks/useFetch";
+import { Book } from "../../data/BookInterface";
+import { Typography } from "@mui/material";
+import { useContext } from "react";
+import { CartContext } from "../../CartContext";
+import { AddShoppingCart } from '@mui/icons-material';
 function Home() {
+  const { addToCart } = useContext(CartContext)
+  // const fetchBooks = useMemo(() => useFetch("http://localhost:3000/books"), [])
+  const { data } = useFetch("http://localhost:3000/books")
+  const books = data as Book[]
+
   return (
     <div className='round-corner'>
       <div className="header">
@@ -36,6 +48,15 @@ function Home() {
           <img src={secondBook} alt="Another Image" className="another-image" />
         </div>
       </div>
+      <Grid container mt={12} justifyContent={"center"} className="frontpage-books">
+        {books?.map(book => (
+          <Grid key={book._id} className="frontpage-book-card">
+            <img src={book.imageUrl} alt={book.title} />
+            <Typography variant="subtitle1" >{book.title}</Typography>
+            <Button onClick={() => addToCart(book)}>ADD TO CART <AddShoppingCart /></Button>
+          </Grid>
+        ))}
+      </Grid>
     </div>
   );
 }
